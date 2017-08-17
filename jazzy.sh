40fca3a7fbe680dd88f8bba58df5b277fa570638
Guihal Gwenn <GGuihal@voyages-sncf.com>
2017-08-17 16:00:03 +0200

    documentation with jazzy

---
+++ jazzy.sh
@@ -0,0 +1,9 @@
+jazzy \
+  --clean \
+  --author "Gwenn Guihal" \
+  --author_url https://voyages-sncf.com \
+  --github_url https://github.com/voyages-sncf-technologies/Collor \
+  --xcodebuild-arguments -workspace,Example/Collor.xcworkspace,-scheme,Collor \
+  --module Collor \
+  --root-url https://github.com/voyages-sncf-technologies/Collor \
+  --output docs
