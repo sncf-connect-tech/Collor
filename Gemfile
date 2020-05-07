65ecdd2e25a22f174f9c704268d8cd64455f17d0
Deffrasnes Ghislain <gdeffrasnes@oui.sncf>
2020-05-07 22:03:36 +0200

    Fix install scripts and deployment target.

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source "https://rubygems.org"
-ruby '~> 2.3'
-
-gem "fastlane", "~> 2.115"
-gem "cocoapods", "~> 1.5"
--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-bundle install
-
-bundle exec pod repo update --verbose
-bundle exec pod install
