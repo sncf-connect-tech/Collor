54b2996cd474dc803fcf14cfc3d139f0e336275e
Gwenn Guihal <gwenn_guihal@connect-tech.sncf>
2025-08-27 11:49:45 +0200

    change organisation name again

--- jazzy.sh
+++ jazzy.sh
@@ -2,8 +2,8 @@ jazzy \
   --clean \
   --author "Voyages-sncf.com" \
   --author_url https://voyages-sncf.com \
-  --github_url https://github.com/voyages-sncf-technologies/Collor \
+  --github_url https://github.com/sncf-connect-tech/Collor \
   --xcodebuild-arguments -workspace,Example/Collor.xcworkspace,-scheme,Collor \
   --module Collor \
-  --root-url https://github.com/voyages-sncf-technologies/Collor \
+  --root-url https://github.com/sncf-connect-tech/Collor \
   --output docs
